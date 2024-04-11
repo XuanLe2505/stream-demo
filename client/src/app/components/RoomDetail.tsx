91b04cd1ad4f9b3b3379a6c383678602ad0c7f48
thoaiky1992 <thoaiky1992@gmail.com>
2024-04-11 11:04:29 +0700

    handle function share screen

--- client/src/app/components/RoomDetail.tsx
+++ client/src/app/components/RoomDetail.tsx
@@ -9,65 +9,112 @@ import VideoCamera from "../common/VideoCamera";
 import VideoCameraSlash from "../common/VideoCameraSlash";
 import useSocket from "../hooks/useSocket";
 import { MediaConnection } from "peerjs";
+import ShareScreenIcon from "../common/ShareScreen";
 
 export default function RoomDetail() {
-    const params = useParams();
-    const { socket } = useSocket();
-    const { stream, setStream, peers, myPeer } = useContext(RoomContext);
-    const [isCamera, setIsCamera] = useState<boolean>(true);
+  const params = useParams();
+  const { socket } = useSocket();
+  const { stream, setStream, peers, myPeer } = useContext(RoomContext);
+  const [isCamera, setIsCamera] = useState<boolean>(true);
+  const [isShareScreen, setIsShareScreen] = useState<boolean>(false);
 
-    const replaceTrack = (peer: MediaConnection, track: MediaStreamTrack) => {
-        if(myPeer) {
-            const sender = peer.peerConnection.getSenders().find(sender => sender.track?.kind === track.kind);
-            sender?.replaceTrack(track);
-        }
-    };
+  const replaceTrack = (peer: MediaConnection, track: MediaStreamTrack) => {
+    if (myPeer) {
+      const sender = peer.peerConnection
+        .getSenders()
+        .find((sender) => sender.track?.kind === track.kind);
+      sender?.replaceTrack(track);
+    }
+  };
 
-    const toggleCamera = async () => {
-        if(stream) {
-            const videoTrack = stream.getVideoTracks()[0];
-            if(isCamera) {
-                videoTrack.enabled = false;
-                videoTrack.stop(); //turn off web cam light indicator
-            } else {
-                const newStream = await navigator.mediaDevices.getUserMedia({
-                    audio: true,
-                    video: true,
-                });
-                const newVideoTrack = newStream.getVideoTracks()[0];
-                stream.removeTrack(videoTrack);
-                stream.addTrack(newVideoTrack);
-                Object.values(peers as peerStateType).forEach(peer => replaceTrack(peer.peer, newVideoTrack));
-            }
-        }
-        setIsCamera(!isCamera);
-    };
+  const toggleCamera = async () => {
+    if (stream) {
+      const videoTrack = stream.getVideoTracks()[0];
+      if (isCamera) {
+        videoTrack.enabled = false;
+        videoTrack.stop(); //turn off web cam light indicator
+      } else {
+        const newStream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
+        const newVideoTrack = newStream.getVideoTracks()[0];
+        stream.removeTrack(videoTrack);
+        stream.addTrack(newVideoTrack);
+        Object.values(peers as peerStateType).forEach((peer) =>
+          replaceTrack(peer.peer, newVideoTrack)
+        );
+      }
+    }
+    setIsCamera(!isCamera);
+  };
 
-    return (
-        <main className="relative min-h-screen w-full p-5">
-            <h1 className=" font-bold text-[20px]">room meeting id: {params.meetingId}</h1>
-            <div className="mt-4 grid grid-cols-2 gap-4">
-                <VideoPlayer stream={stream} isCamera={isCamera} />
-                {Object.values(peers).length > 0 &&
-                    Object.values(peers as peerStateType).map((peer) => <VideoPlayer stream={peer.stream} key={peer.peer.peer} />)}
-            </div>
-            <div className="fixed bottom-0 left-0 right-0 flex justify-center gap-4 py-5">
-                <div className="relative cursor-pointer w-12 h-12 border border-black rounded-full">
-                    <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                        <MicrophoneIcon/>
-                    </div>
-                </div>
-                <div className="relative cursor-pointer w-12 h-12 border border-black rounded-full" onClick={toggleCamera}>
-                    <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                        <VideoCamera/>
-                    </div>
-                </div>
-                <div className="relative cursor-pointer w-12 h-12 border border-black rounded-full">
-                    <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
-                        <VideoCameraSlash/>
-                    </div>
-                </div>
-            </div>
-        </main>
-    );
+  const handleShareScreen = async () => {
+    const isShareScreenTemp = !isShareScreen;
+    let streamTemp: MediaStream;
+    stream?.getTracks().forEach((track) => track.stop());
+    if (isShareScreenTemp) {
+      streamTemp = await navigator.mediaDevices.getDisplayMedia({});
+    } else {
+      streamTemp = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+    }
+    setStream!(streamTemp);
+    Object.values(myPeer?.connections!).forEach((connection: any) => {
+      const videoTrack = streamTemp
+        ?.getTracks()
+        .find((track) => track.kind === "video");
+      connection[0].peerConnection
+        .getSenders()
+        .find((sender: any) => sender.track.kind === "video")
+        .replaceTrack(videoTrack)
+        .catch((err: any) => console.error(err));
+    });
+    setIsShareScreen(isShareScreenTemp);
+  };
+
+  return (
+    <main className="relative min-h-screen w-full p-5">
+      <h1 className=" font-bold text-[20px]">
+        room meeting id: {params.meetingId}
+      </h1>
+      <div className="mt-4 grid grid-cols-2 gap-4">
+        <VideoPlayer stream={stream} isCamera={isCamera} />
+        {Object.values(peers).length > 0 &&
+          Object.values(peers as peerStateType).map((peer) => (
+            <VideoPlayer stream={peer.stream} key={peer.peer.peer} />
+          ))}
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center gap-4 py-5">
+        <div className="relative cursor-pointer w-12 h-12 border border-black rounded-full">
+          <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+            <MicrophoneIcon />
+          </div>
+        </div>
+        <div
+          className="relative cursor-pointer w-12 h-12 border border-black rounded-full"
+          onClick={toggleCamera}
+        >
+          <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+            <VideoCamera />
+          </div>
+        </div>
+        <div className="relative cursor-pointer w-12 h-12 border border-black rounded-full">
+          <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+            <VideoCameraSlash />
+          </div>
+        </div>
+        <div
+          className="relative cursor-pointer w-12 h-12 border border-black rounded-full"
+          onClick={handleShareScreen}
+        >
+          <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2">
+            <ShareScreenIcon />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- client/src/app/reducers/peerReducer.ts
+++ client/src/app/reducers/peerReducer.ts
@@ -1,55 +1,70 @@
 import { MediaConnection } from "peerjs";
-import { ADD_ALL_PEER, ADD_PEER_NAME, ADD_PEER_STREAM, REMOVE_PEER_STREAM } from "./peerActions";
+import {
+  ADD_ALL_PEER,
+  ADD_PEER_NAME,
+  ADD_PEER_STREAM,
+  REMOVE_PEER_STREAM,
+} from "./peerActions";
 
-export type peerStateType = Record<string, { peer: MediaConnection, userName?: string, stream?: MediaStream }>;
+export type peerStateType = Record<
+  string,
+  { peer: MediaConnection; userName?: string; stream?: MediaStream }
+>;
 
 type addPeerStreamType = {
-    type:typeof ADD_PEER_STREAM,
-    payload: { peer: MediaConnection, stream: MediaStream },
-}
+  type: typeof ADD_PEER_STREAM;
+  payload: { peer: MediaConnection; stream: MediaStream };
+};
 type addPeerNameType = {
-    type: typeof ADD_PEER_NAME,
-    payload: { peer: MediaConnection, userName: string },
-}
+  type: typeof ADD_PEER_NAME;
+  payload: { peer: MediaConnection; userName: string };
+};
 type addAllPeersType = {
-    type: typeof ADD_ALL_PEER,
-    payload: { peers: Record<string, { peer: MediaConnection, userName: string }> },
-}
+  type: typeof ADD_ALL_PEER;
+  payload: {
+    peers: Record<string, { peer: MediaConnection; userName: string }>;
+  };
+};
 type removePeerStreamType = {
-    type: typeof REMOVE_PEER_STREAM,
-    payload: { peerId: string },
-}
+  type: typeof REMOVE_PEER_STREAM;
+  payload: { peerId: string };
+};
 
-export type peerActionType = addPeerStreamType | addPeerNameType | addAllPeersType | removePeerStreamType;
+export type peerActionType =
+  | addPeerStreamType
+  | addPeerNameType
+  | addAllPeersType
+  | removePeerStreamType;
 
-export const peersReducer = (state: peerStateType, action : peerActionType) => {
-    switch (action.type) {
-        case ADD_PEER_STREAM:
-            return {
-                ...state,
-                [action.payload.peer.peer]: {
-                    ...state[action.payload.peer.peer],
-                    peer: action.payload.peer,
-                    stream: action.payload.stream,
-                }
-            };
-        case ADD_PEER_NAME:
-            return {
-                ...state,
-                [action.payload.peer.peer]: {
-                    ...state[action.payload.peer.peer],
-                    peer: action.payload.peer,
-                    userName: action.payload.userName,
-                }
-            };
-        case ADD_ALL_PEER:
-            return {...state, ...action.payload.peers};
-        case REMOVE_PEER_STREAM:
-            if(state) {
-                const { [action.payload.peerId]: {}, ...rest } = state;
-                return rest;
-            }
-        default:
-            return { ...state };
-    }
+export const peersReducer = (state: peerStateType, action: peerActionType) => {
+  switch (action.type) {
+    case ADD_PEER_STREAM:
+      return {
+        ...state,
+        [action.payload.peer.peer]: {
+          ...state[action.payload.peer.peer],
+          peer: action.payload.peer,
+          stream: action.payload.stream,
+        },
+      };
+    case ADD_PEER_NAME:
+      return {
+        ...state,
+        [action.payload.peer.peer]: {
+          ...state[action.payload.peer.peer],
+          peer: action.payload.peer,
+          userName: action.payload.userName,
+        },
+      };
+    case ADD_ALL_PEER:
+      return { ...state, ...action.payload.peers };
+    case REMOVE_PEER_STREAM:
+      if (state) {
+        const stateTemp = { ...state };
+        delete stateTemp[action.payload.peerId];
+        return { ...stateTemp };
+      }
+    default:
+      return { ...state };
+  }
 };
